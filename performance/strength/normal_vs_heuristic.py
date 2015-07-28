2d1ba191cadbbaab0ab922a785478210cf0709f4
Kota Ishimoto <ishikota086@gmail.com>
2015-07-28 23:45:23 +0900

    re-tested normal vs heuristic after fixed bug of connectfour model

--- performance/strength/normal_vs_heuristic.py
+++ performance/strength/normal_vs_heuristic.py
@@ -3,5 +3,6 @@ PARENT_PATH = os.getenv('PYMCTS_ROOT')
 import tester
 
 T = tester.Tester()
-T.test(T.UCT, T.HEURISTIC, 100, 10, "out/n_vs_h")
+T.test(T.UCT, T.HEURISTIC, 0, 3, 20, "out/normal_vs_heuristic")
+T.test(T.HEURISTIC, T.UCT, 0, 3, 20, "out/normal_vs_heuristic")
 
